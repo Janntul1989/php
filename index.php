c5dacbfce16920d2b9ce9998f51496aa6366c35f
Jannatul1989 <jchaiti72@gmail.com>
2021-11-27 22:45:19 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Assingment</title>
+</head>
+<body>
+       
+
+
+  
+<form action="index.php" method="POST" >
+ <input type="text" placeholder="First Name" name="fname"  /><br/>
+ <input type="text" placeholder="Last Name" name="lname" /><br/>
+ <input type="email" placeholder="Email" name="email" /><br/>
+ <input type="password" placeholder="Password" name="pwd" /><br/>
+ <input type="submit" value="Submit" />
+</form>
+
+
+<?php
+
+
+ if(isset($_POST['fname'])) {
+  
+  echo "<br/>";
+  echo "First Name: " . $_POST['fname'] . "<br/>";
+  echo "Last Name: " . $_POST['lname'] . "<br/>";
+  echo "Email: " . $_POST['email'] . "<br/>";
+  // echo "Password: " . $_POST['pwd'] . "<br/>";
+  
+   }
+  ?>
+
+</body>
+</html>
